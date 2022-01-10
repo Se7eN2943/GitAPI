2f775dcca74f2a28d255254f944e7e54b818fde4
Se7eN2943 <85510122+Se7eN2943@users.noreply.github.com>
2022-01-10 21:27:41 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,24 +12,27 @@ const debounce = (fn, debounceTime) => {
     }
 };
 
-let counter = 0;
-const fn = () => {
-    counter++;
-};
-
 function searching() {
-    fetch(`https://api.github.com/search/repositories?q=${search.value}&per_page=5&sort=stars`).then(resp => resp.json()).then(res => {
-        resArr = res.items
-        let helpBlank = document.querySelectorAll('.serch_help__blank')
-        if (helpBlank !== null) helpBlank.forEach(el => el.remove())
-        for (const i in resArr) {
-            let elem = document.createElement('div');
-            elem.classList.add("serch_help__blank")
-            elem.textContent = resArr[i].name
-            searchHelp.style.display = "block"
-            searchHelp.appendChild(elem)
-        }
-    })
+    let arrValue = search.value.split('')
+    if (arrValue.length == 0) {
+        searchHelp.style.display = "none"
+        return
+    }
+    if (arrValue.findIndex(i => i !== ' ') !== -1) {
+        fetch(`https://api.github.com/search/repositories?q=${search.value}&per_page=5&sort=stars`).then(resp => resp.json()).then(res => {
+            resArr = res.items
+            let helpBlank = document.querySelectorAll('.serch_help__blank')
+            if (helpBlank !== null) helpBlank.forEach(el => el.remove())
+            for (const i in resArr) {
+                let elem = document.createElement('div');
+                elem.classList.add("serch_help__blank")
+                elem.textContent = resArr[i].name
+                searchHelp.style.display = "block"
+                searchHelp.appendChild(elem)
+                searchHelp.addEventListener('click', add)
+            }
+        })
+    }
 }
 
 function add(e) {
@@ -38,15 +41,18 @@ function add(e) {
     for (const i in resArr) {
         if (e.target.textContent === resArr[i].name) {
             elem.innerHTML = `<div class= text_add> Name: ${resArr[i].name}  <br> Owner: ${resArr[i].owner.login} <br> Stars: ${resArr[i].stargazers_count}</div> <div class=close></div>`
+            blanks.appendChild(elem)
+            input.value = null
+            searchHelp.style.display = "none"
+            blanks.addEventListener('click', removeBlanks)
+            searchHelp.removeEventListener('click', add)
         }
     }
-    blanks.appendChild(elem)
-    input.value = null
-    searchHelp.style.display = "none"
+}
+
+function removeBlanks(e) {
+    if (e.target.classList.value == 'close') e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+    if (blanks.children.length === 0) blanks.removeEventListener('click', removeBlanks)
 }
 
 search.addEventListener('keyup', debounce(searching, 350))
-searchHelp.addEventListener('click', add)
-blanks.addEventListener('click', e => {
-    if (e.target.classList.value == 'close') e.target.parentNode.parentNode.removeChild(e.target.parentNode);
-})
